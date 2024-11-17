660052e7376b565934caa282600c88b2506ab58c
francescodigiamb <francescodigiamb@gmail.com>
2024-11-17 22:13:11 +0100

    step2

--- src/main/java/it/lessons/pizzeria/controller/ControllerPizzeria.java
+++ src/main/java/it/lessons/pizzeria/controller/ControllerPizzeria.java
@@ -1,27 +1,51 @@
 package it.lessons.pizzeria.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import it.lessons.pizzeria.model.Pizza;
 import it.lessons.pizzeria.repository.PizzaRepository;
 
 @Controller
-@RequestMapping ("/pizzeria")
+@RequestMapping("/pizzeria")
 public class ControllerPizzeria {
 
-	@Autowired 
+	@Autowired
 	private PizzaRepository pizzaRepo;
-	
-	@GetMapping 
-	public String index (Model model) {
-		
-		model.addAttribute("pizze", pizzaRepo.findAll());
-		
+
+	@GetMapping
+	public String index(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
+		List<Pizza> lepizze;
+
+		if (keyword != null && !keyword.isBlank()) {
+			lepizze = pizzaRepo.findByDescrizione(keyword);
+			model.addAttribute("keyword", keyword);
+		} else {
+			lepizze = pizzaRepo.findAll();
+		}
+
+		model.addAttribute("pizze", lepizze);
+
 		return "/pizza/index";
 	}
-	
-		
+
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable("id") Integer id, Model model) {
+
+		Optional<Pizza> pizzOptional = pizzaRepo.findById(id);
+		if (pizzOptional.isPresent()) {
+			model.addAttribute("pizze", pizzOptional.get());
+		}
+
+		return "/pizza/show";
+	}
+
 }
--- src/main/java/it/lessons/pizzeria/repository/PizzaRepository.java
+++ src/main/java/it/lessons/pizzeria/repository/PizzaRepository.java
@@ -1,9 +1,13 @@
 package it.lessons.pizzeria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.lessons.pizzeria.model.Pizza;
 
-public interface PizzaRepository extends JpaRepository <Pizza, Integer> {
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+    public List<Pizza> findByDescrizione(String descrizione);
 
 }
